a836bc5f1faa14f772a911f3680ea5a39ca0f98d
Connor O'Brien <connorbobrien@gmail.com>
2023-05-02 09:52:08 -0300

    Remove commented out menu items

--- js/router.js
+++ js/router.js
@@ -4,7 +4,7 @@ import { redirectBasedOnLogin } from "./helpers/auth.js";
 
 export default function router() {
 	const pathname = window.location.pathname;
-	console.log(pathname);
+
 	redirectBasedOnLogin(pathname);
 	buildMenu(pathname);
 
